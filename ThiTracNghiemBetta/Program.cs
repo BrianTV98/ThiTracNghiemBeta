c5e38c5d945bf46c9f5fd15a4d5a969678e23252
Trần Văn Hiếu <49116308+GaNuiGlPk@users.noreply.github.com>
2019-11-23 15:10:03 +0700

    init project

---
+++ ThiTracNghiemBetta/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.UserSkins;
+using ThiTracNghiemBetta.form;
+
+namespace ThiTracNghiemBetta
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BonusSkins.Register();
+            Application.Run(new frmLogin());
+        }
+    }
+}
