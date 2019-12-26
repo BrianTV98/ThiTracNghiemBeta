73285bb884e226b243fa68c7fe72ad55a332436d
Trần Văn Hiếu <tranhieuglpk@gmail.com>
2019-12-26 09:57:12 +0700

    fix

--- ThiTracNghiemBetta/form/frmLogin.cs
+++ ThiTracNghiemBetta/form/frmLogin.cs
@@ -55,8 +55,8 @@ namespace ThiTracNghiemBetta.form
             Program.mHoTen = Program.myReader.GetString(1);
             Program.mNhom = Program.myReader.GetString(2);
             Program.mLoai = Program.myReader.GetString(3);
-            Program.malop = Program.myReader.GetString(4);
-            Program.tenlop = Program.myReader.GetString(5);
+            //Program.malop = Program.myReader.GetString(4);
+           // Program.tenlop = Program.myReader.GetString(5);
 
             Program.myReader.Close();
 
